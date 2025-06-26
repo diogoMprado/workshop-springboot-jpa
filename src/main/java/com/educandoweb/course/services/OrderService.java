095cc59237cb97240ca81b23cadc24023aa70cc6
diogoMprado <dmessiasdoprado@gmail.com>
2025-06-26 18:19:39 -0300

    aula 6 (Order, Instant, ISO 8601) part 2

---
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+    @GetMapping
+    public Order findById(Long id){
+        Optional <Order> obj = repository.findById(id);
+        return obj.get();
+    }
+    }
