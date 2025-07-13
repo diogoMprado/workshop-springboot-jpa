3cdbcf8f369f79ee0afcba693d3622246a61ca3c
diogoMprado <dmessiasdoprado@gmail.com>
2025-07-13 14:49:41 -0300

    aula 15 (User insert)

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,10 +3,8 @@ package com.educandoweb.course.services;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,4 +23,8 @@ public class UserService {
         Optional <User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User obj){
+        return repository.save(obj);
     }
+}
